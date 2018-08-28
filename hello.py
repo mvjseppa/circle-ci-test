522aa34e4dc5fae2d24bd8a2d87aad1b2dc5dc3a
Mikko Seppälä <mikko.seppala@solinor.com>
2018-08-28 15:23:46 +0300

    add unit test for numpy_hello

--- hello.py
+++ hello.py
@@ -1,2 +1,7 @@
+import numpy as np
+
 def hello(name):
     return "hello " + name
+
+def numpy_hello(dimensions):
+    return np.zeros(dimensions, dtype=int)
--- test_hello.py
+++ test_hello.py
@@ -1,12 +1,16 @@
 import unittest
-from hello import hello
+from hello import hello, numpy_hello
 
 class TestHello(unittest.TestCase):
     def test_hello(self):
         self.assertEqual('hello Mikko', hello('Mikko'))
 
-    def test_hello2(self):
-        self.assertTrue(True)
+    def test_numpy_hello(self):
+        nph = numpy_hello((3,4))
+        self.assertEqual((3,4), nph.shape)
+        for i in range(nph.shape[0]):
+            for j in range(nph.shape[1]):
+                self.assertEqual(0, nph[i][j])
 
 if __name__ == '__main__':
     unittest.main()
